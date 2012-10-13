634f00663ca5ee3543665c24d3a08a5ebf9cfcb1
aabes <andi.abes@gmail.com>
2012-10-12 17:26:03 -0700

    add some error checking

--- build/build.rb
+++ build/build.rb
@@ -2,6 +2,13 @@
 require 'yaml'
 
 debug = true #if ENV['DEBUG']
+errs = []
+at_exit { p errs.join("\n") if errs.length > 0}
+
+def check_errors
+  exit 2 if errs && errs.length > 0 
+end
+
 
 repo_data = {}
 
@@ -32,15 +39,19 @@ repo_data.each do |bc_name, repos|
      pip_cache_path = "#{ENV['BC_CACHE']}/files/pip_cache"
      system "mkdir -p #{repos_path}"
      system "mkdir -p #{pip_cache_path}"
-     if File.exists? "#{repos_path}/#{repo_name}.tar.bz2"
+     base_name ="#{repos_path}/#{repo_name}"
+     if File.exists? "#{base_name}.tar.bz2"
        # it seems that pre-cloned repo is already existing
        p "updating repo #{repo_name} from #{origin}" if debug
        system "cd #{repos_path} && tar xf #{repo_name}.tar.bz2"
+       errs << "failed to expand #{repo_name}" unless ::File.exists? "#{base_name}.git"
        p "fetching origin #{origin}" if debug 
-       system "cd #{repos_path}/#{repo_name}.git && git fetch origin"
+       ret = system "cd #{base_name}.git && git fetch origin"
+       errs << "failed to fetch #{base_name}" unless ret
      else
        p "cloning #{origin} to #{repo_name}.git" if debug
-       system "git clone --mirror #{origin} #{repos_path}/#{repo_name}.git"
+       ret = system "git clone --mirror #{origin} #{repos_path}/#{repo_name}.git"
+       errs << "failed to clone #{base_name}" unless ret
      end
      if branches.empty?
        raw_data = `cd #{repos_path}/#{repo_name}.git && git for-each-ref --format='%(refname)' refs/heads`
@@ -48,20 +59,23 @@ repo_data.each do |bc_name, repos|
      end
      p "caching pip requires packages from branches #{branches.join(' ')}" if debug
      system "git clone #{repos_path}/#{repo_name}.git tmp"
+     errs << "failed to create working tree of #{base_name}" unless ret
      if File.exists? "tmp/tools/pip-requires"
        branches.each do |branch|
          system "cd tmp && git checkout origin/#{branch}"
-         system "pip2tgz #{pip_cache_path} -r tmp/tools/pip-requires"
+         ret = system "pip2tgz #{pip_cache_path} -r tmp/tools/pip-requires"
+         errs << "failed download pips for #{base_name}" unless ret
        end
      end
      system "rm -fr tmp"
-     system "dir2pi #{pip_cache_path}"
+     ret = system "dir2pi #{pip_cache_path}"
+     errs << "failed to package pip reqs" unless ret
      p "packing #{repo_name}.git to #{repo_name}.tar.bz2" if debug
      system "cd #{repos_path} && tar cjf #{repo_name}.tar.bz2 #{repo_name}.git/"
      p "cleaning #{repo_name}.git" if debug
-     system "rm -fr #{repos_path}/#{repo_name}.git"
+     #system "rm -fr #{repos_path}/#{repo_name}.git"
    end
   end
 end
-
+check_errors
 p "git repos staging is complete now" if debug
